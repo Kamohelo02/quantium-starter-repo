a290544cd6416d075c7615ed7bc8edb988a54810
Kamohelo Maphela <kamohelomaphela04@gmail.com>
2026-02-08 18:51:27 -0800

    Test Suite Automated

---
+++ run_tests.ps1
@@ -0,0 +1,16 @@
+﻿# run_tests.ps1
+
+# Activate virtual environment
+.\venv\Scripts\Activate.ps1
+
+# Run tests
+python -m pytest test_app.py -v
+
+# Capture exit code
+$TEST_EXIT_CODE = $LASTEXITCODE
+
+# Deactivate
+deactivate
+
+# Exit with test's exit code
+exit $TEST_EXIT_CODE
